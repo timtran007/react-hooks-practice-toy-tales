786c92d03b89f0ee05799d210161e1c6cd432139
Tim <timtran007@gmail.com>
2022-12-15 19:39:17 -0600

    commit tree 438a77add836b66e776ddf3f0ab67d7b785d2b42

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
@@ -6,10 +6,19 @@ import ToyContainer from "./ToyContainer";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [toys, setToys] = useState([])
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
   }
+
+  useEffect(() => {
+    fetch('http://localhost:3001/toys')
+    .then(resp => resp.json())
+    .then(data => setToys(data))
+  }, [])
+
+  console.log('toys', toys)
 
   return (
     <>
@@ -18,7 +27,7 @@ function App() {
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer toys={toys}/>
     </>
   );
 }
